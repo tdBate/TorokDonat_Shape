3e65e2c040993d55e9ab299b65924cb6bb1ea426
Torok Donat <diak@petrik.hu>
2025-03-21 12:45:46 +0100

    Feladat kész

--- Shape/Program.cs
+++ Shape/Program.cs
@@ -1,14 +1,62 @@
-﻿using System.Threading.Channels;
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
 
 namespace Shape
 {
 	internal class Program
 	{
+		static void KeruletVsTeruletLyukasztas(Shape s1) 
+		{
+			if (s1.Area() > s1.Perimeter())
+			{
+				s1.MakeHoley();
+			}
+		}
+
+		static void RectangleLetrehozas(int a, int b)
+		{
+			if (a == b)
+			{
+				new Square(false, "red", a, b);
+			}
+			else 
+			{
+				new Rectangle(false, "red", a, b);
+			}
+		}
+
+		static Shape MaxTerulet(Shape[] shapes)
+		{
+			Shape max = shapes[0];
+			for (int i = 1; i < shapes.Length; i++)
+			{
+				if (max.Area() < shapes[i].Area())
+				{
+					max = shapes[i];
+				}
+			}
+			return max;
+
+		}
+
+		static void Feladat()
+		{
+			Shape[] shapes =
+				{
+				new Square(false, "red", 6,6),
+				new Circle("white",5),
+				new Rectangle(true,"grey",1,2),
+				new Square(true, "purple", 4,2),
+				new Circle("orange",2),
+				};
+
+		}
+
 		static void Main(string[] args)
 		{
 			Rectangle rectangle = new Rectangle(false,"red",1,2);
             //Rectangle rectangle = new();
-            Console.WriteLine(rectangle);
+            //Console.WriteLine(rectangle);
 
 			Shape shape = rectangle;
 
@@ -23,6 +71,8 @@ namespace Shape
 
 			Rectangle rectangle1 = square;
 			Shape shape3 = square;
+
+			Feladat();
         }	
 	}
 }
